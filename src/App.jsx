215af4b4768000896df1a78adf27101424ba988e
1ceyMan <nourachraf112@gmail.com>
2025-11-22 22:05:41 +0100

    Formated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState ,createContext} from 'react'
+import { useState, createContext } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,20 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import LogIn from './Formulaires/LogInFolder/logIn.jsx';
 import SignIn from './Formulaires/SignInFolder/SignIn.jsx';
-export const context = createContext(); 
+export const context = createContext();
 function App() {
 
-  const [dateBirth,setDateBirth] = useState();
-  const [isFormValid,setIsFormValid] = useState(false)
-  const [HaveAccount,setIsHaveAccount] = useState(true)
+  const [dateBirth, setDateBirth] = useState();
+  const [isFormValid, setIsFormValid] = useState(false)
+  const [HaveAccount, setIsHaveAccount] = useState(true)
 
   return (
     <>
-    <context.Provider value={{HaveAccount,setIsHaveAccount,isFormValid,setIsFormValid,dateBirth,setDateBirth}}>
-      <div className={`auth-wrapper ${HaveAccount ? "slide-in" : "slide-out"}`}>
-        {HaveAccount ? <LogIn/> : <SignIn/>}
-      </div>
-      </context.Provider> 
+      <context.Provider value={{ HaveAccount, setIsHaveAccount, isFormValid, setIsFormValid, dateBirth, setDateBirth }}>
+        <div className={`auth-wrapper ${HaveAccount ? "slide-in" : "slide-out"}`}>
+          {HaveAccount ? <LogIn /> : <SignIn />}
+        </div>
+      </context.Provider>
     </>
   )
 }
